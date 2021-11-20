123a0740cc8836510e6dafb7b52eebf65676fc3b
Alejandro Moreno <61042882+JandroK@users.noreply.github.com>
2021-11-20 18:55:43 +0100

    Mouse Picking
    And draw debug ray
    Primitives also are selectables

--- Fire_Engine/Engine/Source/Editor.cpp
+++ Fire_Engine/Engine/Source/Editor.cpp
@@ -294,7 +294,7 @@ update_status Editor::ImGuiMenuBar()
 
 void Editor::NewScene()
 {
-	static_cast<Inspector*>(app->editor->GetTab(TabType::INSPECTOR))->gameObjectSelected = nullptr;
+	SetGameObjectSelected(nullptr);
 	app->scene->CleanUp(); //Clean GameObjects 
 	app->scene->Init();
 	app->camera->ReStartCamera();
@@ -307,6 +307,7 @@ void Editor::PrimitiveMenuItem()
 		PrimitiveCube cubePrim = PrimitiveCube();
 		cubePrim.InnerMesh();
 		cubePrim.mesh->LoadToMemory();
+		cubePrim.mesh->GenerateBounds();
 
 		app->scene->CreatePrimitive("Cube", cubePrim.mesh);
 	}
@@ -315,6 +316,7 @@ void Editor::PrimitiveMenuItem()
 		PrimitiveSphere spherePrim = PrimitiveSphere();
 		spherePrim.InnerMesh();
 		spherePrim.mesh->LoadToMemory();
+		spherePrim.mesh->GenerateBounds();
 
 		app->scene->CreatePrimitive("Sphere", spherePrim.mesh);
 	}
@@ -323,6 +325,7 @@ void Editor::PrimitiveMenuItem()
 		PrimitiveCylinder cylinderPrim = PrimitiveCylinder();
 		cylinderPrim.InnerMesh();
 		cylinderPrim.mesh->LoadToMemory();
+		cylinderPrim.mesh->GenerateBounds();
 
 		app->scene->CreatePrimitive("Cylinder", cylinderPrim.mesh);
 	}
@@ -331,6 +334,7 @@ void Editor::PrimitiveMenuItem()
 		PrimitivePyramid pyramidPrim = PrimitivePyramid();
 		pyramidPrim.InnerMesh();
 		pyramidPrim.mesh->LoadToMemory();
+		pyramidPrim.mesh->GenerateBounds();
 
 		app->scene->CreatePrimitive("Pyramid", pyramidPrim.mesh);
 	}
@@ -361,10 +365,12 @@ Tab* Editor::GetTab(TabType type)
 
 GameObject* Editor::GetGameObjectSelected()
 {
-	Inspector* inspector = static_cast<Inspector*>(GetTab(TabType::INSPECTOR));
-	return inspector->gameObjectSelected;
+	return static_cast<Inspector*>(GetTab(TabType::INSPECTOR))->gameObjectSelected;
 }
-
+void Editor::SetGameObjectSelected(GameObject* obj)
+{
+	static_cast<Inspector*>(app->editor->GetTab(TabType::INSPECTOR))->gameObjectSelected = obj;
+}
 
 void Editor::DockSpaceOverViewportCustom(ImGuiViewport* viewport, ImGuiDockNodeFlags dockspaceFlags, ImVec2 position, ImVec2 size, const ImGuiWindowClass* windowClass)
 {
