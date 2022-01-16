d05bc806a70359670a340b7413779e065d754506
Alejandro Moreno <61042882+JandroK@users.noreply.github.com>
2022-01-16 16:37:09 +0100

    ScreenShoots

--- Fire_Engine/Engine/Source/External/Extras/IconsFontAwesome5.h
+++ Fire_Engine/Engine/Source/External/Extras/IconsFontAwesome5.h
@@ -3,11 +3,11 @@
 // for use with https://github.com/FortAwesome/Font-Awesome/blob/master/webfonts/fa-regular-400.ttf, https://github.com/FortAwesome/Font-Awesome/blob/master/webfonts/fa-solid-900.ttf
 #pragma once
 
-//#define FONT_ICON_FILE_NAME_FAR "../Output/Assets/fa-regular-400.ttf"
-#define FONT_ICON_FILE_NAME_FAR "Assets/fa-regular-400.ttf"
+#define FONT_ICON_FILE_NAME_FAR "../Output/Assets/fa-regular-400.ttf"
+//#define FONT_ICON_FILE_NAME_FAR "Assets/fa-regular-400.ttf"
 
-//#define FONT_ICON_FILE_NAME_FAS "../Output/Assets/fa-solid-900.ttf"
-#define FONT_ICON_FILE_NAME_FAS "Assets/fa-solid-900.ttf"
+#define FONT_ICON_FILE_NAME_FAS "../Output/Assets/fa-solid-900.ttf"
+//#define FONT_ICON_FILE_NAME_FAS "Assets/fa-solid-900.ttf"
 
 #define ICON_MIN_FA 0xe005
 #define ICON_MAX_FA 0xf8ff
